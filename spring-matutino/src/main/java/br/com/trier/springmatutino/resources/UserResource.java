690deebba9842376eb9c9edd8793dcbf6e5c3dfe
Lucas Duarte <duarte4lucas@gmail.com>
2023-06-15 11:52:48 -0300

    começando exercicio campeonato

--- spring-matutino/src/main/java/br/com/trier/springmatutino/resources/UserResource.java
+++ spring-matutino/src/main/java/br/com/trier/springmatutino/resources/UserResource.java
@@ -1,51 +1,57 @@
 package br.com.trier.springmatutino.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.trier.springmatutino.domain.User;
+import br.com.trier.springmatutino.services.UserService;
 
 @RestController
 @RequestMapping (value = "/usuarios")
 public class UserResource {
 	
-	List<User> lista = new ArrayList<User>();
-	
-	public UserResource() {
-		lista.add(new User(1, "Usuario 1", "usuario1@gmail.com", "123"));
-		lista.add(new User(2, "Usuario 2", "usuario2@gmail.com", "123"));
-		lista.add(new User(3, "Usuario 3", "usuario3@gmail.com", "123"));
-	}
-	
-	@GetMapping 
-	public 	List<User> listAll() {
-		return lista;
-	}
-	
-	@GetMapping ("/{codigo}")
-	public ResponseEntity<User> findById(@PathVariable(name = "codigo") Integer codigo) {
-		User u = lista.stream()
-				.filter(user -> user.getId().equals(codigo))
-				.findAny()
-				.orElse(null);
-		return u != null ? ResponseEntity.ok(u) : ResponseEntity.noContent().build();
-	
-	}
+	@Autowired
+	private UserService service;
 	
 	@PostMapping
-	public User insert(@RequestBody User u) {
-		u.setId(lista.size()+1);
-		lista.add(u);
-		return u;
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		User newUser = service.salvar(user);
+		return newUser!=null ? ResponseEntity.ok(newUser) : ResponseEntity.badRequest().build();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> buscaPorCodigo(@PathVariable Integer id) {
+		User user = service.findById(id);
+		return user!=null ? ResponseEntity.ok(user) : ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<User>> listarTodos() {
+		List<User> lista = service.listAll();
+		return lista.size()>0 ? ResponseEntity.ok(lista) : ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user) {
+		user.setId(id);
+		user = service.update(user);
+		return user != null ? ResponseEntity.ok(user) : ResponseEntity.badRequest().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}
 	
 }
