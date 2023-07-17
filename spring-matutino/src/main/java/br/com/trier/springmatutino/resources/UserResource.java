fcce8052e3ea1746d7d3c3c1e87bb7efaf3d9b39
Lucas Duarte <duarte4lucas@gmail.com>
2023-07-17 12:01:12 -0300

    alterações spring

--- spring-matutino/src/main/java/br/com/trier/springmatutino/resources/UserResource.java
+++ spring-matutino/src/main/java/br/com/trier/springmatutino/resources/UserResource.java
@@ -25,26 +25,26 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@PostMapping
 	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO user) {
 		return  ResponseEntity.ok(service.salvar(new User(user)).toDto());
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> buscaPorCodigo(@PathVariable Integer id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok(user.toDto());
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> listarTodos() {
 		return ResponseEntity.ok(service.listAll().stream().map(user -> user.toDto()).toList());
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@PutMapping("/{id}")
 	public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
 		User user = new User(userDTO);
@@ -53,20 +53,20 @@ public class UserResource {
 		return ResponseEntity.ok(user.toDto());
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.ok().build();
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/name/{name}")
 	public ResponseEntity<List<UserDTO>> buscaPorNome(@PathVariable String name){
 		return ResponseEntity.ok(service.findByNameIgnoreCase(name).stream().map(user -> user.toDto()).toList());
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/name/termina/{name}")
 	public ResponseEntity<List<UserDTO>> buscaPorNomeTerminaCom(@PathVariable String name){
 		return ResponseEntity.ok(service.findByNameEndsWith(name).stream().map(user -> user.toDto()).toList());
